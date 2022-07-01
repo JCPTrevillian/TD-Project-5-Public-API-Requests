aaf23b9c345e20d3100ddcaf383a7b5a315c8c25
Jennifer Trevillian <95890754+JCPTrevillian@users.noreply.github.com>
2022-06-30 20:47:41 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$(document).ready(() => {
+    
+    //DOB in randomuser.me/api lists DOB as YEAR/MO/DAY 4/2/2, restructure as MO/DY/YR 2/2/2 so it appears in US bday order on the ui 
+    const birthdayRestructure = (birthday) => {
+        let birthdayRestruct = new Date(birthday).toLocaleDateString('en-US', { year: '2-digit', month: 'numeric', day: 'numeric' });
+        return birthdayRestruct;
+    }
+    
+    //Setting up variables with unstuctured data in randomuser so I can easily call variables with newly structured data in successive steps 
+    const employeeData = (data) => {
+        let image = data.picture.large;
+        let name = `${data.name.first} ${data.name.last}`;
+        let email = data.email;
+        let city = data.location.city;
+        let username = data.login.username;
+        let cell = data.phone;
+        let fullAddress = `${data.location.street}, ${city}, ${data.location.state} ${data.location.postcode}`;
+        let birthday = birthdayRestructure(data.dob);
+        
+  
